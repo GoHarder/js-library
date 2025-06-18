d06aaf80e80b9801cf9bc37876b906765f9afeff
Gregory Harder <goharder83@gmail.com>
2025-06-18 14:54:02 -0500

    Added array module.

---
+++ packages/array/module.ts
@@ -0,0 +1,16 @@
+/**
+ * A library of array related functions.
+ * @module @moss/array
+ */
+
+// MARK: Library
+// --------------------------------------------------------------------------------------
+/**
+ * Creates an incrementing range of numbers.
+ * @param start The start of the range.
+ * @param end The end of the range.
+ * @param step The step size.
+ */
+function range(start: number, end: number, step: number = 1): number[] {
+  return Array.from({ length: (end - start) / step + 1 }, (_, i) => start + i * step);
+}
